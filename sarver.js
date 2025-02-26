d96cf0352c9a80c98dde2a47f5379fef0e40ae20
Shubham Verma <91955778+Sv26@users.noreply.github.com>
2025-02-26 13:16:08 +0530

    Update sarver.js

--- sarver.js
+++ sarver.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 3001;
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("API is live on http://localhost:3000/api/users");
 });
